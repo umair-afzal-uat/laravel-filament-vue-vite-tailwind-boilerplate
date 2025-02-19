dac5118cf579bcc0005b0fd3f97ff1ad291d01b5
umair-afzal-uat <umair.afzal.uat@gmail.com>
2025-02-19 22:47:48 +0500

    MY

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        vue()
+        vue(),
     ],
+    server: {
+        host: '0.0.0.0',  // Listen on all network interfaces
+        port: 5173,
+        hmr: {
+            host: 'localhost',  // Use localhost for hot module replacement
+        },
+        watch: {
+            usePolling: true, // Helps with file updates inside Docker
+        },
+    },
 });
