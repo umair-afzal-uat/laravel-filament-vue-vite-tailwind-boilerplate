68715f157378f069d3199d229ea4b1637b382806
umair-afzal-uat <umair.afzal.uat@gmail.com>
2025-02-20 19:14:50 +0500

    Setup Docker and Public Pages

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="text-center text-3xl font-bold text-blue-600">
-        Welcome to My Laravel + Vue + Filament App!
-    </div>
+    <router-view></router-view>
 </template>
+
+<script setup>
+</script>
